e962133ab50690f2a6b660adebb849377c8f0075
pankajparkar <parkarpankaj3@gmail.com>
2021-08-21 13:23:51 +0800

    Installed @angular/material

--- apps/admin/src/app/app.module.ts
+++ apps/admin/src/app/app.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatListModule } from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 
 import { AppComponent } from './app.component';
+// import { RouterModule, Routes } from '@angular/router';
+
+// const routes: Routes = [
+//   {path: 'product/list', component: ProductListComponent,},
+//   {path: 'product/details/1', component: ProductComponent},
+//   {path: '', redirectTo: 'product/list', pathMatch: 'full' },
+// ];
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    MatListModule, MatDividerModule, MatIconModule, MatCardModule,
+    // RouterModule.forRoot(routes)
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
